49b2080f3a6ec83aeb2dae7589737d444e63f119
moisesdurandelrazo <moises6737@gmail.com>
2022-07-04 11:17:56 -0500

    moy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [gifs, setGifs] = useState(GIFS);
+
+  useEffect (function (){
+    console.log ('efecto ejecutado  ')
+  })
+
+
+  // const state=useState([])
+  // const value =state[0]
+  // const updateValue = state[1]
+
+  //const [value, updateValue]=useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          gifs.map(singleGif => <img src =  {singleGif}/>)
+        }
+      </section>
     </div>
   );
 }
